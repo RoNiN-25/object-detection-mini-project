776f4604a1ecaab111454892fd5a8ed5814202af
RoNiN-25 <aravindiyer25@gmail.com>
2019-11-14 22:05:39 +0530

    Added human follow

--- yolo_opencv.py
+++ yolo_opencv.py
@@ -1,6 +1,6 @@
 import cv2
-import argparse
 import numpy as np
+import time
 
 
 
@@ -22,70 +22,163 @@ def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
     cv2.rectangle(img, (x,y), (x_plus_w,y_plus_h), color, 2)
 
     cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    
 
+
+def number_points(a,boxes):
+    
+   cnt = []
+   number = []
+    
+    
+   for i in boxes:
+        x1 = i[0]
+        y1 = i[1]
+        x2 = x1+i[2]
+        y2 = y1
+        x3 = x2
+        y3 = y2+i[3]
+        x4 = x1
+        y4 = y3
+        L = [[x1,y1],[x2,y2],[x3,y3],[x4,y4]]
+        cnt.append(np.array(L).reshape((-1,1,2)).astype(np.int32))
+     
+   for i in cnt:
+        num = 0
+        for n in range(a[0].shape[0]):
+            if(cv2.pointPolygonTest(i, (a[0][n],a[1][n]), False) == -1.0):
+                num=num+1
+        number.append(num)
+    
+   if(number !=[]):
+       k = number.index(max(number))
+       return cnt[k].tolist()
+   else:
+       return None
+       
+        
+   
+    
+scale = 0.00392
+classes = None
+
+
+lower_color = np.array([110, 50, 50])
+upper_color = np.array([130 , 255 , 255])
+
+with open("yolov3.txt", 'r') as f:
+    classes = [line.strip() for line in f.readlines()]
+COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+
+hhh=0
+sum=0.0
 cap = cv2.VideoCapture(0)
 while (1):
+   # start = time.time()
     _,image = cap.read()
+    
+    
 
-    Width = image.shape[1]
-    Height = image.shape[0]
-    scale = 0.00392
-
-    classes = None
-
-    with open("yolov3.txt", 'r') as f:
-        classes = [line.strip() for line in f.readlines()]
-
-    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
-
-    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
-
-    blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
-
-    net.setInput(blob)
-
-    outs = net.forward(get_output_layers(net))
-
-    class_ids = []
-    confidences = []
-    boxes = []
-    conf_threshold = 0.5
-    nms_threshold = 0.4
+    
+    if(hhh%10==0):
+        Width = image.shape[1]
+        Height = image.shape[0]
 
 
-    for out in outs:
-        for detection in out:
-            scores = detection[5:]
-            class_id = np.argmax(scores)
-            confidence = scores[class_id]
-            if confidence > 0.5:
-                center_x = int(detection[0] * Width)
-                center_y = int(detection[1] * Height)
-                w = int(detection[2] * Width)
-                h = int(detection[3] * Height)
-                x = center_x - w / 2
-                y = center_y - h / 2
-                class_ids.append(class_id)
-                confidences.append(float(confidence))
-                boxes.append([x, y, w, h])
+        
+
+        blob = cv2.dnn.blobFromImage(image, scale, (416,416), (0,0,0), True, crop=False)
+
+        net.setInput(blob)
+
+        outs = net.forward(get_output_layers(net))
+        
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+        res = cv2.bitwise_and(image,image, mask= mask)
+
+        
+
+        class_ids = []
+        confidences = []
+        boxes = []
+        conf_threshold = 0.5
+        nms_threshold = 0.4
 
 
-    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                if(class_id == 0):
+                    confidence = scores[class_id]
+                    if confidence > 0.5:
+                        center_x = int(detection[0] * Width)
+                        center_y = int(detection[1] * Height)
+                        w = int(detection[2] * Width)
+                        h = int(detection[3] * Height)
+                        x = center_x - w / 2
+                        y = center_y - h / 2
+                        class_ids.append(class_id)
+                        confidences.append(float(confidence))
+                        boxes.append([x, y, w, h])
 
-    for i in indices:
-        i = i[0]
-        box = boxes[i]
-        x = box[0]
-        y = box[1]
-        w = box[2]
-        h = box[3]
-        draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
 
-    cv2.imshow("object detection", image)
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+        for i in indices:
+            i = i[0]
+            box = boxes[i]
+            x = box[0]
+            y = box[1]
+            w = box[2]
+            h = box[3]
+            draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x+w), round(y+h))
+        
+       
+        
+        
+        
+        #cv2.imshow('frame',image)
+        cv2.imshow('mask',mask)
+        #cv2.imshow('res',res)
+        
+
+        a=np.where(mask==255)
+        fff = number_points(a,boxes)
+
+        
+
+        
+        if(fff is not None):
+            x1 = round(fff[0][0][0])
+            y1 = round(fff[0][0][1])
+            x2 = round(fff[2][0][0])
+            y2 = round(fff[2][0][1])
+            cv2.rectangle(image, (x1,y1), (x2,y2), (255,255,255), 5)
+        else:
+            print("No object to follow")
+        
+        
+        
+        
+        cv2.imshow("object detection", image)
+    
+        
+
+    
+   # end = time.time()
+   # sum = sum+(end-start)
+
+    
+    hhh=hhh+1
+
+    
     k=cv2.waitKey(5) & 0xFF
     if k==27:
         break
 
 
-#cv2.imwrite("object-detection.jpg", image)
+cap.release()
 cv2.destroyAllWindows()
